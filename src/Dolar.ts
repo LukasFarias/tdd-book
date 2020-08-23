8871c2d5ae7a5858967c6d022c20122746288437
Lucas Farias <lucasfariasdacruz@gmail.com>
2020-08-23 00:06:41 -0400

    Privacidade

--- src/Dolar.ts
+++ src/Dolar.ts
@@ -1,10 +1,26 @@
+interface Object {
+    amount: Dolar;
+}
+
+interface DolarDTO {
+    value: number;
+}
+
+
+
 export default class Dolar {
-    amount: number;
+
+    private amount: number;
+
     constructor(amount: number){
         this.amount = amount;
     }
+
     public times(multipler: number){
         return new Dolar(this.amount * multipler);
     }
 
+    public equals(object: Dolar): boolean{
+        return this.amount === object.amount;
+    }
 }
--- src/tests/book.test.ts
+++ src/tests/book.test.ts
@@ -3,10 +3,13 @@ import Dolar from "../Dolar";
 describe('Livro TDD', () => {
     test('teste multiplicador', () => {
         const five: Dolar = new Dolar(5);
-        let product: Dolar = five.times(2);
-        expect(10).toBe(product.amount);
-        product = five.times(3);
-        expect(15).toBe(product.amount);
+        expect(new Dolar(10)).toEqual(five.times(2));
+        expect(new Dolar(15)).toEqual(five.times(3));
     });
+
+    test('test equelity', () => {
+        expect(true).toBe(new Dolar(5).equals(new Dolar(5)));
+        expect(false).toBe(new Dolar(5).equals(new Dolar(6)));
+    })
 });
 
